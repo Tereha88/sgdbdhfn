e67f7be0e1dc5516661d856fcaa651a1dd11de3a
Tereha88 <54bara@mail.ru>
2021-10-21 21:58:19 +1100

    w

--- src/Game. В Game.kt
+++ src/Game. В Game.kt
@@ -5,13 +5,20 @@ fun main() {
     val isImmortal = false // Бессмертие
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal// Аура
     val auraColor = if (auraVisible) "GREEN" else "NONE"// Условия видимости ауры
-    println(auraColor)
-    val healthStatus = if (healthPoints == 100) " is in excellent condition!"
-     else if (healthPoints in 90..99) " has a few scratches."
-     else if (healthPoints in 75..99)
-        if (isBlessed) " has some minor wounds but is healing quite quickly!"
-        else " has some minor wounds."
-     else if (healthPoints in 15..74) " looks pretty hurt."
-     else " is in awful condition!"
+    val healthStatus = when (healthPoints) {
+        100 -> "is in excellent condition!"
+        in 90..99 -> "has a few scratches."
+        in 75..89 -> if (isBlessed) {
+            "has some minor wounds but is healing quite quickly!"
+        }
+         else {
+            "has some minor wounds."
+        }
+        in 15..74 -> "looks pretty hurt."
+        else -> "is in awful condition!"
+    }
      println("$name$healthStatus")// Состояние игрока
+    println("(Aura: $auraColor) " +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
 }
